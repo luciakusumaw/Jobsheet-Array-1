fbba53e15cfaab10a7f556405496e3357b518e6c
luciakusumaw <itsmelucia27@gmail.com>
2025-11-20 14:38:34 +0700

    Experiment 3

---
+++ arrayAverageScore13.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class arrayAverageScore13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score [] = new int [10];
+        double total = 10;
+        double average;
+
+        for (int i = 0; i<score.length; i++) {
+            System.out.print("Enter Student score " + (i+1) + ": ");
+            score[i] = sc.nextInt();
+        } 
+        for (int i = 0; i<score.length; i++) {
+            total += score[i];
+        }
+        average = total/score.length;
+        System.out.print("The Class Average Score is: " + average);
+    }
+}
